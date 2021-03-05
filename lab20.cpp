bba030b9787e8ceb01ee0e2dab286e8fb41dfee9
khomsan <khomsan_su@cmu.ac.th>
2021-03-05 16:58:19 +0700

    complete

--- lab20.cpp
+++ lab20.cpp
@@ -1,3 +1,4 @@
+#include"lab20.h"
 // [Missing Code 1] Include header file.
   
 int main(){
@@ -9,7 +10,11 @@ int main(){
 	Unit hero("Hero",name);
 	
 	Equipment sword(0,8,4);
+	Equipment axes(0,16,-3);
+	Equipment shield(0,-1,7);
+	Equipment armor(25,-2,2);
 	// [Missing Code 2]  Create Equipment axes, shield and armor here
+	
 
 	
 	
@@ -18,6 +23,11 @@ int main(){
 	cout << "Please selet your equipment: ";
 	cin >> eq;
 	// [Missing Code 3] Equip a selected equipment to the hero. 
+	if(eq == '1') hero.equip(&sword);
+	if(eq == '2') hero.equip(&axes);
+	if(eq == '3') hero.equip(&shield);
+	if(eq == '4') hero.equip(&armor);
+	
 
 	
 	
@@ -58,7 +68,10 @@ int main(){
 			cout << "Please select your equipment: ";
 			cin >> eq;
 			// [Missing Code 3] Equip a selected equipment to the hero.
-
+			if(eq == '1') hero.equip(&sword);
+			if(eq == '2') hero.equip(&axes);
+			if(eq == '3') hero.equip(&shield);
+			if(eq == '4') hero.equip(&armor);
 			
 		}
 		
